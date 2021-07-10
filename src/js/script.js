4aa15209c9bcfd49574d8341bd7cddce33ffe348
Radik Abubjazov <abubjazov@mail.ru>
2021-07-10 22:22:53 +0300

    свёрстана секция contacts

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.progress__percent');
+const lines = document.querySelectorAll('.progress__bar-fill');
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
